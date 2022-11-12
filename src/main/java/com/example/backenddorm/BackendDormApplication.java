3bcb9df35ab0f8862add3a74f2c8536c283eb092
Nattawat Samsee <62070067@kmitl.ac.th>
2022-11-12 15:21:56 +0700

    first commit

---
+++ src/main/java/com/example/backenddorm/BackendDormApplication.java
@@ -0,0 +1,13 @@
+package com.example.backenddorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backenddorm/BackendDormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
